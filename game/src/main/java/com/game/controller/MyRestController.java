95de633110a68f374d199fd8e3aac58f268e9bcf
shved <vad.shvedov@mail.ru>
2021-05-10 10:28:04 +0300

    another one

--- game/src/main/java/com/game/controller/MyRestController.java
+++ game/src/main/java/com/game/controller/MyRestController.java
@@ -1,8 +1,16 @@
 package com.game.controller;
 
+import com.game.entity.Profession;
+import com.game.entity.Race;
+import com.game.model.Player;
 import com.game.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MyRestController {
